b1229d00b2d1be1a235f6386ebd09431d919f2e3
RomanAzeem <romanazeem007@gmail.com>
2020-04-09 21:34:17 +0500

    filter Tasks added

--- app.js
+++ app.js
@@ -57,3 +57,15 @@ function clearTasks(e) {
     }
   }
 }
+// Filter Task function
+function filterTask(e) {
+  const text = e.target.value.toLowerCase();
+  document.querySelectorAll('.collection-item').forEach(function (task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
+}
